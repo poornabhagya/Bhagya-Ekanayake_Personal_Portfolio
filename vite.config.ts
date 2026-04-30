8ec495f746f338ed187ac1a451a48eb62ef961c3
poornabhagya <poornaekanayake1@gmail.com>
2026-04-30 13:12:22 +0530

    fix vite config file and yml file

--- vite.config.ts
+++ vite.config.ts
@@ -17,7 +17,7 @@ function figmaAssetResolver() {
 }
 
 export default defineConfig({
-  base: './Bhagya-Ekanayake_Personal_Portfolio/',
+  base: '/Bhagya-Ekanayake_Personal_Portfolio/',
   plugins: [
     figmaAssetResolver(),
     // The React and Tailwind plugins are both required for Make, even if
